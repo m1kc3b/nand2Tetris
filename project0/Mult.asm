fa28096fce42bf5eadb05aaf1587c7d70439ddaa
m1ckc3b <cebulski.mi@gmail.com>
2024-11-08 17:06:04 +0100

    write and test it in CPU emulator

--- project0/Mult.asm
+++ project0/Mult.asm
@@ -1,1 +1,34 @@
 // R2 = R0 * R1
+@i
+M=0
+@sum
+M=0
+
+(LOOP)
+  // if i > R1 goto STOP
+  @i 
+  D=M
+  @R1
+  D=D-M
+  @STOP
+  D;JEQ
+  // sum = sum + R0
+  @R0
+  D=M
+  @sum
+  M=D+M
+  // i++
+  @i 
+  M=M+1
+  @LOOP
+  0;JMP
+
+(STOP)
+  @sum
+  D=M
+  @R2
+  M=D
+
+(END)
+  @END
+  0;JMP
