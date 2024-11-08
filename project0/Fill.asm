a8f09b8f5671bc17a8866063d4c3a23a65abf2e5
m1ckc3b <cebulski.mi@gmail.com>
2024-11-08 18:50:02 +0100

    init

--- project0/Fill.asm
+++ project0/Fill.asm
@@ -2,3 +2,51 @@
 // SCREEN is blacken
 // else
 // SCREEN is cleared
+@KBD
+
+// if KBD > 0 goto BLACKEN
+@BLACKEN
+M;JGT
+
+
+(BLACKEN)
+  @i 
+  D=M
+  @KBD
+  D=D-M
+  @END
+  D;JEQ
+
+  @SCREEN
+  D=M
+  @i 
+  A=D+M
+  M=-1
+
+  @i 
+  M=M+1
+
+  0;JMP
+
+(CLEARED)
+  @i 
+  D=M
+  @KBD
+  D=D-M
+  @END
+  D;JEQ
+
+  @SCREEN
+  D=M
+  @i 
+  A=D+M
+  M=0
+
+  @i 
+  M=M+1
+
+  0;JMP
+
+(END)
+  @END
+  0;JMP
