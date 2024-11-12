edad3c3e4f58462ea7e1f223ad8ae25450748eda
m1ckc3b <cebulski.mi@gmail.com>
2024-11-12 11:03:29 +0100

    init struct + methods

---
+++ project07/src/parser.rs
@@ -0,0 +1,28 @@
+use std::fs::File;
+
+
+struct Parser {
+  file: File
+}
+
+impl Parser {
+  fn new(file: File) -> Self {
+    todo!()
+  }
+
+  fn hasMoreLines() {
+    todo!()
+  }
+
+  fn commandType() {
+    todo!()
+  }
+
+  fn arg1() -> String {
+    todo!()
+  }
+
+  fn arg2() {
+    
+  }
+}
