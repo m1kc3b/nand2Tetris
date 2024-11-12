5b85f26a920ecd9a378f3c67ad8fcfb421ee4ffd
m1ckc3b <cebulski.mi@gmail.com>
2024-11-12 10:13:57 +0100

    init

---
+++ project07/src/stack.rs
@@ -0,0 +1,30 @@
+
+struct Stack<T> {
+  data: Vec<T>,
+  sp: u16
+}
+
+impl<T> Stack<T> {
+  fn new() -> Self {
+    Self { 
+      data: Vec::new(),
+      sp: 0,
+     }
+  }
+
+  fn push(&mut self, value: T) {
+    self.data.push(value);
+    self.sp += 1;
+  }
+
+  fn pop(&mut self) -> Option<T> {
+    self.sp -= 1;
+    self.data.pop()
+  }
+
+  fn add(&mut self) {
+    let y = self.data.pop().unwrap();
+    let x = self.data.pop().unwrap();
+    self.data.push(x + y);
+  }
+}
