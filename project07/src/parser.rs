ef1d57238a2977062225a928abb8249d53a501c6
m1ckc3b <cebulski.mi@gmail.com>
2024-11-12 11:18:13 +0100

    modify self into &self in the methods

--- project07/src/parser.rs
+++ project07/src/parser.rs
@@ -23,27 +23,27 @@ impl Parser {
   }
 
   // Are there more lines in the input ?
-  fn hasMoreLines(self) {
+  fn hasMoreLines(&self) {
     todo!()
   }
 
   // Reads the next command from the input and makes it the current command
-  fn advance(self) {
+  fn advance(&self) {
     todo!()
   }
 
   // Returns a constant representing the type of the current command.
-  fn commandType(self) -> CommandType {
+  fn commandType(&self) -> CommandType {
     todo!()
   }
 
   // Returns the first argument of the current command
-  fn arg1(self) -> String {
+  fn arg1(&self) -> String {
     todo!()
   }
 
   // Returns the second argument of the current command
-  fn arg2(self) {
+  fn arg2(&self) {
     todo!()
   }
 }
