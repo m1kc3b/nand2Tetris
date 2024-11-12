5f2da486130597d48cc36395833dc1610c1ff96f
m1ckc3b <cebulski.mi@gmail.com>
2024-11-12 11:44:49 +0100

    init vmtranslator

--- project07/src/main.rs
+++ project07/src/main.rs
@@ -1,7 +1,15 @@
-mod stack;
 mod parser;
-mod codeWriter;
+mod code_writer;
 
 fn main() {
     println!("Hello, world!");
+
+}
+
+fn vmtranslator() {
+    // The program gets the name of the input source file, say Prog.vm, from the command-line argument.
+    // It constructs a Parser
+    // And constructs an output file, Prog.asm
+    // Loop: iteartes through the VM commands in the input file
+
 }
