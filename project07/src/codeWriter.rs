261da5241e3258e3a63a67fcce56d756e8c58773
m1ckc3b <cebulski.mi@gmail.com>
2024-11-12 11:17:24 +0100

    init

---
+++ project07/src/codeWriter.rs
@@ -0,0 +1,27 @@
+use std::fs::File;
+
+
+struct codeWriter {
+  file: File
+}
+
+impl codeWriter {
+  fn new(file: File) -> Self {
+    todo!()
+  }
+
+  // Writes to the output file the assembly code that implements the given arithmetic-logical command
+  fn writeArithmetic(&mut self, command: String) {
+    todo!()
+  }
+
+  // Writes to the output file the assembly code that implements the given push or pop command
+  fn writePushPop(&mut self, command: String, segment: String, index: i16) {
+    todo!()
+  }
+
+  // Closes the output file/stream
+  fn closes(&self) {
+    todo!()
+  }
+}
