56037a7da1f3be749027300fd28c884badb15f15
m1ckc3b <cebulski.mi@gmail.com>
2024-11-12 11:11:02 +0100

    add comments

--- project07/src/parser.rs
+++ project07/src/parser.rs
@@ -1,28 +1,49 @@
 use std::fs::File;
 
+enum CommandType {
+  C_ARITHMETIC,
+  C_PUSH,
+  C_POP,
+  C_LABEL,
+  C_GOTO,
+  C_IF,
+  C_FUNCTION,
+  C_RETURN,
+  C_CALL
+}
 
 struct Parser {
   file: File
 }
 
 impl Parser {
+  // Opens the input file/stream, and gets ready to parse it
   fn new(file: File) -> Self {
     todo!()
   }
 
-  fn hasMoreLines() {
+  // Are there more lines in the input ?
+  fn hasMoreLines(self) {
     todo!()
   }
 
-  fn commandType() {
+  // Reads the next command from the input and makes it the current command
+  fn advance(self) {
     todo!()
   }
 
-  fn arg1() -> String {
+  // Returns a constant representing the type of the current command.
+  fn commandType(self) -> CommandType {
     todo!()
   }
 
-  fn arg2() {
-    
+  // Returns the first argument of the current command
+  fn arg1(self) -> String {
+    todo!()
+  }
+
+  // Returns the second argument of the current command
+  fn arg2(self) {
+    todo!()
   }
 }
