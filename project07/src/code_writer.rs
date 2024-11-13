353e9bc76c9009658e42e5673b6a9110a6bf8acf
m1ckc3b <cebulski.mi@gmail.com>
2024-11-13 16:23:54 +0100

    init write_arithmetic & write_push_pop

--- project07/src/code_writer.rs
+++ project07/src/code_writer.rs
@@ -1,27 +1,42 @@
-use std::fs::File;
-
+use std::{fs::File, io::Write};
 
 struct CodeWriter {
   file: File
 }
 
 impl CodeWriter {
-  fn new(file: File) -> Self {
-    todo!()
+  fn new(filename: String) -> Result<Self, String> {
+    let file = File::create_new(format!("{}.asm", filename));
+
+    if let Ok(f) = file {
+      return Ok(Self { file: f })
+    }
+    Err("Impossible de créer le fichier".to_string())
   }
 
   // Writes to the output file the assembly code that implements the given arithmetic-logical command
   fn write_arithmetic(&mut self, command: String) {
-    todo!()
+    let file = self.file;
+    file.write(command);
   }
 
   // Writes to the output file the assembly code that implements the given push or pop command
   fn write_push_pop(&mut self, command: String, segment: String, index: i16) {
-    todo!()
+    let file = self.file;
+    // Check the command_type (C_PUSH or C_POP)
+    let command = format!("{command} {segment} {index}");
+    file.write(command);
   }
 
   // Closes the output file/stream
   fn closes(&self) {
     todo!()
   }
+}
+
+
+mod tests {
+    use super::*;
+
+
 }
