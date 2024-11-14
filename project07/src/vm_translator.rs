39e0b35b895969a34ee892a203e52635cda0a62b
m1ckc3b <cebulski.mi@gmail.com>
2024-11-14 16:08:47 +0100

    TODO: handling Option<&str>

--- project07/src/vm_translator.rs
+++ project07/src/vm_translator.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 use crate::{code_writer, parser};
 use crate::parser::CommandType;
 
@@ -5,32 +7,26 @@ use crate::parser::CommandType;
 pub fn vmtranslator(arguments: Vec<String>) {
   let file = &arguments[1];
   // It constructs a Parser
-  let mut new_parser = parser::Parser::new(file).unwrap();
+  let mut parser = parser::Parser::new(file).unwrap();
 
   // And constructs an output file, Prog.asm
   let mut output_file = code_writer::CodeWriter::new(file).unwrap();
 
   // Loop: iteartes through the VM commands in the input file
-  while new_parser.has_more_lines() == true {
-      // commands[index]
-      let command = &new_parser.commands[new_parser.index];
-      // handle comments & empty lines
-      if command.starts_with("//") | command.is_empty() {
-        new_parser.advance();
+  while parser.has_more_lines() == true {
+      let command_type = parser.command_type();
+
+      match command_type {
+        Some(CommandType::C_PUSH | CommandType::C_POP) => {
+          let segment = parser.arg1();
+          let index = parser.arg2();
+          output_file.write_push_pop(command_type, &segment, index);
+        },
+        Some(CommandType::C_ARITHMETIC) => {
+          let command = parser.commands[parser.index].split(" ").collect()[0];
+          output_file.write_arithmetic(&command);
+        },
+        _ => parser.advance(),
       }
-      // command_type
-      let cmd_type = new_parser.command_type();
-      // get args1
-      let segment = new_parser.arg1();
-      // get args2
-      let index = new_parser.arg2();
-      // if C_PUSH or C_POP -> code_writer.write_push_pop + parser.advance
-      if let Some(CommandType::C_PUSH) | Some(CommandType::C_POP) = &cmd_type {
-        output_file.write_push_pop(&command, &segment, index);
-        new_parser.advance();
-      }
-      // if C_arithmetic -> code_writer.write_arithmetic + parser.advance
-      output_file.write_arithmetic(command);
-      new_parser.advance();
   }
 }
