2f6954a7fd5a0c17d8e0762a146adc4c33bbe9f9
m1ckc3b <cebulski.mi@gmail.com>
2024-11-19 10:38:44 +0100

    init CLI

--- project05-assembler/src/main.rs
+++ project05-assembler/src/main.rs
@@ -1,5 +1,15 @@
+use std::env;
+use std::fs;
+
 mod parser;
 
 fn main() {
-    println!("Hello, world!");
+    // $ HackAssembler Prog.asm
+    let args: Vec<String> = env::args().collect();
+    // Open file
+    let file = fs::read_to_string(&args[1]).unwrap_or_else(|err| {
+        format!("An error occured with the file: {}", err)
+});
+    println!("{}", file);
+    // parser(file)
 }
