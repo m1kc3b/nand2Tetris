f929bab54233a6f187a7d26373dc15ac5aba4435
m1ckc3b <cebulski.mi@gmail.com>
2024-11-19 10:45:33 +0100

    Check if files is provided

--- project05-assembler/src/main.rs
+++ project05-assembler/src/main.rs
@@ -1,11 +1,17 @@
 use std::env;
 use std::fs;
+use std::process;
 
 mod parser;
 
 fn main() {
     // $ HackAssembler Prog.asm
     let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("No files provided!");
+        process::exit(1);
+    }
     // Open file
     let file = fs::read_to_string(&args[1]).unwrap_or_else(|err| {
         format!("An error occured with the file: {}", err)
