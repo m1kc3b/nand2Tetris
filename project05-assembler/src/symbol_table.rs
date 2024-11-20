299945c4bc96fa233122d1693375de8eb43f788c
m1ckc3b <cebulski.mi@gmail.com>
2024-11-20 15:03:18 +0100

    init & test add_entry()

--- project05-assembler/src/symbol_table.rs
+++ project05-assembler/src/symbol_table.rs
@@ -1,7 +1,23 @@
+#[derive(Debug, PartialEq, Eq)]
+struct Symbol {
+  name: String,
+  address: usize
+}
 
+#[derive(Debug, PartialEq, Eq)]
+pub struct SymbolTable {
+  entries: Vec<Symbol>
+}
 
+impl SymbolTable {
+  pub fn new() -> Self {
+    Self { entries: Vec::new() }
+  }
 
-
+  pub fn add_entry(&mut self, name: String, address: usize) {
+    self.entries.push(Symbol { name, address });
+  }
+}
 
 
 
@@ -13,7 +29,15 @@ mod tests {
   use super::*;
 
   #[test]
-  fn it_works() {
-    
+  fn should_create_and_symbol_table() {
+    let symbol_table = SymbolTable::new();
+    assert_eq!(symbol_table, SymbolTable { entries: Vec::new()})
+  }
+
+  #[test]
+  fn should_add_new_entry_to_the_symbol_table() {
+    let mut symbol_table = SymbolTable::new();
+    symbol_table.add_entry("test".to_string(), 1);
+    assert_eq!(symbol_table, SymbolTable { entries: vec![Symbol { name: "test".to_string(), address: 1 }]})
   }
 }
