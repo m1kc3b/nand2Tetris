46cc7b64e3dda671d229cb032577585011e210da
m1ckc3b <cebulski.mi@gmail.com>
2024-11-20 11:11:51 +0100

    init & test new() method

--- project05-assembler/src/parser.rs
+++ project05-assembler/src/parser.rs
@@ -6,42 +6,42 @@ enum InstructionType {
   LInstruction,
 }
 
-struct Parser {
-  file: String,
+pub struct Parser {
+  pub input: String,
 }
 
 impl Parser {
-  fn new(&self, file: String) -> Self {
-      Self { file }
+  pub fn new(input: String) -> Self {
+      Self { input }
   }
 
-  fn has_more_lines(&self) -> bool {
-      todo!()
-  }
+//   fn has_more_lines(&self) -> bool {
+//       todo!()
+//   }
 
-  fn advance(&self) {
-      todo!()
-  }
+//   fn advance(&self) {
+//       todo!()
+//   }
 
-  fn instruction_type(&self) -> InstructionType {
-      todo!()
-  }
+//   fn instruction_type(&self) -> InstructionType {
+//       todo!()
+//   }
 
-  fn symbol(&self) -> String {
-      todo!()
-  }
+//   fn symbol(&self) -> String {
+//       todo!()
+//   }
 
-  fn dest(&self) -> String {
-      todo!()
-  }
+//   fn dest(&self) -> String {
+//       todo!()
+//   }
 
-  fn comp(&self) -> String {
-      todo!()
-  }
+//   fn comp(&self) -> String {
+//       todo!()
+//   }
 
-  fn jump(&self) -> String {
-      todo!()
-  }
+//   fn jump(&self) -> String {
+//       todo!()
+//   }
 }
 
 
@@ -49,9 +49,14 @@ impl Parser {
 #[cfg(test)]
 mod tests {
 
+    use std::fs::read_to_string;
+
     use super::*;
 
-    fn get_file_from_cli_args() {
-        todo!()
+    #[test]
+    fn init_new_parser_with_test_file() {
+        let input = read_to_string("asm-files/test.asm").unwrap();
+        let parser = Parser::new(input);
+        assert_eq!(parser.input, "// Computes R1=1+...+R0".to_string());
     }
 }
