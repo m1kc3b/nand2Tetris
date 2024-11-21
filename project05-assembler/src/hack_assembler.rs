316fa4b752c05329da8e0cba40720b785de37666
m1ckc3b <cebulski.mi@gmail.com>
2024-11-21 16:13:39 +0100

    pass tests for first pass of execute()

--- project05-assembler/src/hack_assembler.rs
+++ project05-assembler/src/hack_assembler.rs
@@ -1,19 +1,18 @@
 use std::fs::File;
 use std::io;
 
-use crate::parser::Parser;
-use crate::symbol_table::SymbolTable;
+use crate::{parser::{self, InstructionType, Parser}, symbol_table::SymbolTable};
 
-pub struct HackAssembler<'a> {
+pub struct HackAssembler {
     parser: Parser,
-    symbol_table: SymbolTable<'a>,
+    symbol_table: SymbolTable,
     pass: usize,
     current_line: usize,
     output_file: File,
 }
 
-impl<'a> HackAssembler<'a> {
-    pub fn new(filename: &'a str) -> Result<Self, io::Error> {
+impl HackAssembler {
+    pub fn new(filename: &str) -> Result<Self, io::Error> {
         let parser = Parser::new(filename)?;
         let symbol_table = SymbolTable::new();
         let output_file = File::create(filename)?;
@@ -34,8 +33,13 @@ impl<'a> HackAssembler<'a> {
         // get the line
         // check instruction_type
         // add label to symbol_table
-        while let Some(line) = self.parser.advance() {
-          println!("{:?}", line.unwrap())
+        while let Some(Ok(line)) = self.parser.advance() {
+          // get label instruction
+          if let Some(InstructionType::LInstruction) = self.parser.instruction_type(&line) {
+            // insert into symbol_table
+            let symbol = self.parser.symbol(line).unwrap();
+            self.symbol_table.add_entry(symbol, self.parser.get_line_count().unwrap() +1);
+          }
         }
 
         // Second pass:
@@ -50,12 +54,30 @@ impl<'a> HackAssembler<'a> {
 
 #[cfg(test)]
 mod tests {
+    use crate::hack_assembler;
+
     use super::HackAssembler;
 
 
     #[test]
-    fn should_return_each_line() {
-      let mut hack_assembler = HackAssembler::new("Add.asm").unwrap();
+    fn symbol_table_should_contains_loop_and_stop() {
+      let mut hack_assembler = HackAssembler::new("Sum1ToN.asm").unwrap();
       hack_assembler.execute();
+      assert_eq!(hack_assembler.symbol_table.contains("LOOP"), true);
+      assert_eq!(hack_assembler.symbol_table.contains("STOP"), true);
+    }
+
+    #[test]
+    fn symbol_table_should_contains_loop_with_address_6() {
+      let mut hack_assembler = HackAssembler::new("Sum1ToN.asm").unwrap();
+      hack_assembler.execute();
+      assert_eq!(hack_assembler.symbol_table.get_address("LOOP"), Some(6));
+    }
+
+    #[test]
+    fn symbol_table_should_contains_stop_with_address_21() {
+      let mut hack_assembler = HackAssembler::new("Sum1ToN.asm").unwrap();
+      hack_assembler.execute();
+      assert_eq!(hack_assembler.symbol_table.get_address("STOP"), Some(21));
     }
 }
