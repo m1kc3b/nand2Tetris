bd524bac86b147c6b2868585c0937f4d92faa58e
m1ckc3b <cebulski.mi@gmail.com>
2024-11-21 20:42:35 +0100

    TODO: implement CInstruction in the Second pass of execute()

--- project05-assembler/src/hack_assembler.rs
+++ project05-assembler/src/hack_assembler.rs
@@ -1,5 +1,6 @@
-use std::{fs::File, io::Write};
-use std::io::{self, Error};
+use std::fs::OpenOptions;
+use std::io::Error;
+use std::io::Write;
 
 use crate::{
     parser::{InstructionType, Parser},
@@ -9,26 +10,28 @@ use crate::{
 pub struct HackAssembler {
     parser: Parser,
     symbol_table: SymbolTable,
-    output_file: File,
+    output_file: String,
+    filename: String,
 }
 
 impl HackAssembler {
-    pub fn new(filename: &str) -> Result<Self, io::Error> {
+    pub fn new(filename: &str) -> Result<Self, Error> {
         let parser = Parser::new(filename)?;
         let symbol_table = SymbolTable::new();
         let name: Vec<&str> = filename.split(".").collect();
-        let output_file = File::create(format!("hack-files/{}.hack", name[0]))?;
+        let file_name = name[0];
+        let output_file = format!("hack-files/{}.hack", file_name);
         Ok(Self {
             parser,
             symbol_table,
             output_file,
+            filename: file_name.to_string(),
         })
     }
 
     pub fn execute(&mut self) -> Result<(), Error> {
-        // First pass:
+        // First pass: TODO create a Lines
         while let Some(Ok(line)) = self.parser.advance() {
-          println!("ENTER INTO 1ST PASS");
             match self.parser.instruction_type(&line) {
                 Some(InstructionType::LInstruction) => {
                     let symbol = self.parser.symbol(line).unwrap();
@@ -36,105 +39,83 @@ impl HackAssembler {
                         .add_entry(symbol, self.parser.get_line_count().unwrap() + 1);
                 }
                 Some(InstructionType::AInstruction) => {
-                  let symbol = self.parser.symbol(line).unwrap();
-                  self.symbol_table
+                    let symbol = self.parser.symbol(line).unwrap();
+                    self.symbol_table
                         .update_entry(symbol, self.parser.get_line_count().unwrap());
-                },
+                }
                 _ => continue,
             }
         }
 
-        // Second pass: TODO -> re-init Lines !!!
-        while let Some(Ok(line)) = self.parser.advance() {
-          println!("ENTER INTO 2ND PASS");
-            match self.parser.instruction_type(&line) {
-                Some(InstructionType::AInstruction) => {
-                  let symbol = self.parser.symbol(line).unwrap();
-                  // symbol == label -> get_address -> binary
-                  if let Some(add) = self.symbol_table.get_address(&symbol) {
-                    let binary = format!("{:016b}", &add);
-                    write!(self.output_file, "{}", binary)?;
-                  }
-                  // symbol == num -> binary
-                  let parsed: usize = symbol.parse().unwrap();
-                  let binary = format!("{:016b}", parsed);
-                  write!(self.output_file, "{}", binary)?;
-                  // insert in output_file
-                },
-                Some(InstructionType::CInstruction) => {
-                  // concatenate dest + comp + jump
-                  // insert in output_file
-                },
-                _ => continue,
+        // Second pass:
+        match self.parser.reinitialize_lines(format!("{}.asm", &self.filename).as_str()) {
+            Ok(()) => {
+                while let Some(Ok(line)) = self.parser.advance() {
+                  let mut file = OpenOptions::new().append(true).create(true).open(&self.output_file)?;
+                    match self.parser.instruction_type(&line) {
+                        Some(InstructionType::AInstruction) => {
+                            let symbol = self.parser.symbol(line).unwrap();
+
+                            // symbol == label -> get_address -> binary
+                            if let Some(add) = self.symbol_table.get_address(&symbol) {
+                                let binary = format!("{:016b}\n", &add);
+                                file.write_all(binary.as_bytes())?;
+                            }
+
+                            // symbol == num -> binary
+                            if let Ok(num) =  symbol.parse::<i32>() {
+                                file.write_all(format!("{:016b}", num).as_bytes())?;
+                            }
+                          }
+                        Some(InstructionType::CInstruction) => { // TODO !!!!
+                            // concatenate dest + comp + jump
+                            // insert in output_file
+                        }
+                        _ => continue,
+                    }
+                }
             }
+            Err(err) => eprintln!("Error with reinitialization: {}", err),
         }
+
         Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::path::Path;
     use super::*;
+    use std::path::Path;
 
     #[test]
-    fn symbol_table_should_contains_loop_stop_i_sum() {
+    fn symbol_table_should_exists_and_contains_entries_with_sum1ton_file() {
         let mut hack_assembler = HackAssembler::new("Sum1ToN.asm").unwrap();
         let _ = hack_assembler.execute();
         assert_eq!(hack_assembler.symbol_table.contains("LOOP"), true);
         assert_eq!(hack_assembler.symbol_table.contains("STOP"), true);
         assert_eq!(hack_assembler.symbol_table.contains("i"), true);
         assert_eq!(hack_assembler.symbol_table.contains("sum"), true);
-    }
-
-    #[test]
-    fn symbol_table_should_contains_loop_with_address_6() {
-        let mut hack_assembler = HackAssembler::new("Sum1ToN.asm").unwrap();
-        let _ = hack_assembler.execute();
         assert_eq!(hack_assembler.symbol_table.get_address("LOOP"), Some(6));
-    }
-
-    #[test]
-    fn symbol_table_should_contains_stop_with_address_21() {
-        let mut hack_assembler = HackAssembler::new("Sum1ToN.asm").unwrap();
-        let _ = hack_assembler.execute();
         assert_eq!(hack_assembler.symbol_table.get_address("STOP"), Some(21));
-    }
-
-    #[test]
-    fn symbol_table_should_contains_i_with_address_16() {
-        let mut hack_assembler = HackAssembler::new("Sum1ToN.asm").unwrap();
-        let _ = hack_assembler.execute();
         assert_eq!(hack_assembler.symbol_table.get_address("i"), Some(16));
-    }
-
-    #[test]
-    fn symbol_table_should_contains_sum_with_address_17() {
-        let mut hack_assembler = HackAssembler::new("Sum1ToN.asm").unwrap();
-        let _ = hack_assembler.execute();
         assert_eq!(hack_assembler.symbol_table.get_address("sum"), Some(21));
     }
 
-    // #[test]
-    // fn show_symbol_table() {
-    //   let mut hack_assembler = HackAssembler::new("Sum1ToN.asm").unwrap();
-    //     let _ = hack_assembler.execute();
-    //     println!("{:#?}", hack_assembler.symbol_table)
-    // }
+    #[test]
+    fn symbol_table_should_exists_with_test_file() {
+      let mut hack_assembler = HackAssembler::new("test.asm").unwrap();
+        let _ = hack_assembler.execute();
+        assert_eq!(hack_assembler.symbol_table.contains("i"), true);
+        assert_eq!(hack_assembler.symbol_table.get_address("i"), Some(1));
+        assert_eq!(Path::new("hack-files/test.hack").exists(), true);
+    } 
 
     #[test]
-    fn output_file_should_exists() {
-      let mut hack_assembler = HackAssembler::new("Sum1ToN.asm").unwrap();
+    fn symbol_table_should_exists_with_add_file() {
+      let mut hack_assembler = HackAssembler::new("Add.asm").unwrap();
       let _ = hack_assembler.execute();
-      assert_eq!(Path::new("hack-files/Sum1ToN.hack").exists(), true);
-    }
-
-    // #[test]
-    // fn output_file_should_contains_a_instructions_only() {
-    //   let mut hack_assembler = HackAssembler::new("Sum1ToN.asm").unwrap();
-    //   let _ = hack_assembler.execute();
-    //   assert_eq!(
-    //   read_to_string("Sum1ToN.hack").unwrap(),
-    //   "0000000000000001\n0000000000000110\n0000000000001100\n0000000000001110\n0000000000010000\n0000000000010101");
-    // }
+        assert_eq!(hack_assembler.symbol_table.contains("THIS"), true);
+        assert_eq!(hack_assembler.symbol_table.get_address("THIS"), Some(3));
+        assert_eq!(Path::new("hack-files/Add.hack").exists(), true);
+    } 
 }
