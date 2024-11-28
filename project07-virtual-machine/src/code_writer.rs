4e6439b706c9a2f8a0868500e70237e3a9312cd8
m1ckc3b <cebulski.mi@gmail.com>
2024-11-28 18:31:54 +0100

    TODO: implement &Some(CommandType::C_pop) in write_push_pop()

--- project07-virtual-machine/src/code_writer.rs
+++ project07-virtual-machine/src/code_writer.rs
@@ -1,30 +1,144 @@
 use crate::parser::CommandType;
 
+fn write_push_pop(
+    command: &Option<CommandType>,
+    segment: Option<&str>,
+    index: Option<i16>,
+) -> String {
+    let mut output_string: String;
+    match command {
+        &Some(CommandType::C_push) => {
+            let pust_to_stack = format!("@SP\nA=M\nM=D\n@SP\nM=M+1");
+            match segment {
+                Some("constant") => {
+                    output_string = format!("@{}\nD=A\n{}", index.unwrap(), pust_to_stack);
+                }
+                Some("local") => {
+                    output_string = format!(
+                        "@{}\nD=A\n@LCL\nA=M+D\nM=A\nD=M\n{}",
+                        index.unwrap(),
+                        pust_to_stack
+                    );
+                }
+                Some("argument") => {
+                    output_string = format!(
+                        "@{}\nD=A\n@ARG\nA=M+D\nM=A\nD=M\n{}",
+                        index.unwrap(),
+                        pust_to_stack
+                    );
+                }
+                Some("this") => {
+                    output_string = format!(
+                        "@{}\nD=A\n@THIS\nA=M+D\nM=A\nD=M\n{}",
+                        index.unwrap(),
+                        pust_to_stack
+                    );
+                }
+                Some("that") => {
+                    output_string = format!(
+                        "@{}\nD=A\n@THAT\nA=M+D\nM=A\nD=M\n{}",
+                        index.unwrap(),
+                        pust_to_stack
+                    );
+                }
+                Some("pointer") => {
+                    if index.unwrap() == 0 {
+                        output_string = format!("@THIS\nA=M+D\nM=A\nD=M\n{}", pust_to_stack);
+                    } else {
+                        output_string = format!("@THAT\nA=M+D\nM=A\nD=M\n{}", pust_to_stack);
+                    }
+                }
+                Some("temp") => {
+                    output_string = format!(
+                        "@{}\nD=A\n@TEMP\nA=M+D\nM=A\nD=M\n{}",
+                        index.unwrap(),
+                        pust_to_stack
+                    );
+                }
+                Some("static") => {
+                    output_string = format!("Foo.{}\n{}", index.unwrap(), pust_to_stack);
+                }
+                _ => output_string = format!("ERROR"),
+            }
+        }
+        _ => {
+          
+        }
+    }
+    output_string
+}
+
 #[cfg(test)]
 mod tests {
 
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn should_create_a_new_code_writer() {
-    let output_string: String;
-    let (command, segment, index) = (&Some(CommandType::C_push), Some("constant"), Some(10));
+    #[test]
+    fn should_translate_push_command_into_asm() {
+        let mut output_string: String;
+        let (command, segment, index) = (&Some(CommandType::C_push), Some("static"), Some(7));
 
-    match command {
-      Some(cmd) => {
-        match cmd {
-          CommandType::C_arithmetic => {
-            output_string = format!("arithmetic ops", )
-          },
-          _ => {
-            output_string = format!("push/pop ops")
-          }
+        if let &Some(CommandType::C_push) = command {
+            let pust_to_stack = format!("@SP\nA=M\nM=D\n@SP\nM=M+1");
+            match segment {
+                Some("constant") => {
+                    output_string = format!("@{}\nD=A\n{}", index.unwrap(), pust_to_stack);
+                }
+                Some("local") => {
+                    output_string = format!(
+                        "@{}\nD=A\n@LCL\nA=M+D\nM=A\nD=M\n{}",
+                        index.unwrap(),
+                        pust_to_stack
+                    );
+                }
+                Some("argument") => {
+                    output_string = format!(
+                        "@{}\nD=A\n@ARG\nA=M+D\nM=A\nD=M\n{}",
+                        index.unwrap(),
+                        pust_to_stack
+                    );
+                }
+                Some("this") => {
+                    output_string = format!(
+                        "@{}\nD=A\n@THIS\nA=M+D\nM=A\nD=M\n{}",
+                        index.unwrap(),
+                        pust_to_stack
+                    );
+                }
+                Some("that") => {
+                    output_string = format!(
+                        "@{}\nD=A\n@THAT\nA=M+D\nM=A\nD=M\n{}",
+                        index.unwrap(),
+                        pust_to_stack
+                    );
+                }
+                Some("pointer") => {
+                    if index.unwrap() == 0 {
+                        output_string = format!("@THIS\nA=M+D\nM=A\nD=M\n{}", pust_to_stack);
+                    } else {
+                        output_string = format!("@THAT\nA=M+D\nM=A\nD=M\n{}", pust_to_stack);
+                    }
+                }
+                Some("temp") => {
+                    output_string = format!(
+                        "@{}\nD=A\n@TEMP\nA=M+D\nM=A\nD=M\n{}",
+                        index.unwrap(),
+                        pust_to_stack
+                    );
+                }
+                Some("static") => {
+                    output_string = format!("Foo.{}\n{}", index.unwrap(), pust_to_stack);
+                }
+                _ => output_string = format!("ERROR"),
+            }
+        } else {
+            output_string = format!("None");
         }
-      },
-      None => output_string = format!("Error")
+
+        // assert_eq!(output_string, "@7\nD=A\n@TEMP\nA=M+D\nM=A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1".to_string());
+        assert_eq!(
+            output_string,
+            "Foo.7\n@SP\nA=M\nM=D\n@SP\nM=M+1".to_string()
+        );
     }
-
-    assert_eq!(output_string, "push/pop ops".to_string());
-
-  }
 }
