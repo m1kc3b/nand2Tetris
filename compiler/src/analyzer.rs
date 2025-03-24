6ec8f852782772eb43ed34690f0ea2a1ddf4faa2
m1kc3b <m1kc3b@gmail.com>
2025-03-24 14:16:51 +0100

    impl analyze

--- compiler/src/analyzer.rs
+++ compiler/src/analyzer.rs
@@ -1,9 +1,45 @@
+use std::fs;
+use std::io;
+use std::path::Path;
 
+use crate::tokenizer::tokenize;
+use crate::compilation_engine::CompilationEngine;
 
-// 1. creates a tokenizer from Xxx.jack input file
-// 2. creates an output file named Xxx.xml
-// 3. uses the tokenizer and the compile_engine to parse the input file and write the parsed code to the output file
+pub fn analyze(source: &str) -> Result<(), io::Error> {
+    let path = Path::new(source);
 
+    if path.is_file() {
+        // C'est un fichier `.jack`, on le traite directement
+        if let Some(ext) = path.extension() {
+            if ext == "jack" {
+                process_file(path)?;
+            }
+        }
+    } else if path.is_dir() {
+        // C'est un dossier, on traite chaque fichier `.jack`
+        for entry in fs::read_dir(path)? {
+            let entry = entry?;
+            let file_path = entry.path();
 
-// source = file | folder
-pub fn analyze(source: &str) {}
+            if let Some(ext) = file_path.extension() {
+                if ext == "jack" {
+                    process_file(&file_path)?;
+                }
+            }
+        }
+    } else {
+        return Err(io::Error::new(io::ErrorKind::NotFound, "Invalid source path"));
+    }
+
+    Ok(())
+}
+
+fn process_file(file_path: &Path) -> Result<(), io::Error> {
+    // 1. Tokenizer : handle tokens
+    let tokens = tokenize(file_path.to_str().unwrap())?;
+
+    // 2. CompilationEngine : handle xml file
+    let _engine = CompilationEngine::new(tokens, file_path.to_str().unwrap())?;
+
+    Ok(())
+}
